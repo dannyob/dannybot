28b3660446a6ed67d94a1486f290799c3e90cb98
Danny O'Brien <danny@eff.org>
2015-12-18 00:02:09 -0800

    Validate JSON of folder configuration

--- src/mail.js
+++ src/mail.js
@@ -1,8 +1,30 @@
 // notmuch = require('notmuch');
-var fs = require('fs');
+const fs = require('fs')
+const _ = require('underscore')
 
+let folders = {}
+// read in the folder list
 fs.readFile(process.env.HOME + '/.config/dannybot/mail_config.js', 
         function (err, data) { 
-            if (err) throw err; 
-            module.exports.folders = JSON.parse(data);
-        });
+            if (err) throw err
+            folders = JSON.parse(data)
+        })
+
+// enforce some structure on folders
+function validate_folders(mf) {
+    let r = true
+    _.map(mf, function (v, k, l) {
+        console.log(v)
+        if (v.length !== 3 ) { console.log(v + " not 3"); r = false }
+        if ((v[0] !== "threads") && (v[0] !== "messages")) { console.log(v[0] + "not thready or messagey") ; r = false }
+    })
+   return r
+}
+
+// exports
+module.exports.folders = folders
+
+if (process.env.NODE_ENV === 'test') {
+    module.exports._private = {}
+    module.exports._private.validate_folders = validate_folders
+}
--- test/mail.js
+++ test/mail.js
@@ -1,10 +1,32 @@
 /* eslint func-names: 0 */
-import { expect } from 'chai';
-import mail from '../src/mail';
-
+import { expect } from 'chai'
+import mail from '../src/mail'
 
 describe('mail.folders', () => {
-  it('should return an array', () => {
-    expect(mail.folders).to.be.an('object');
-  });
-});
+    it('should return an object', () => {
+        expect(mail.folders).to.be.an('object')
+    })
+})
+
+const legit_folder = { 'inbox' : ["threads", "tag:foo", "inbox"] }
+const not_legit_folder1 = { "messbox" : ["threads", "wrongsize"] }
+const not_legit_folder2 = { "messbox" : ["scorcher", "otherwise", "okay"] }
+
+describe('mail._private.validate_folders', () => { 
+    it('should return true on a legit folder structure', () => {
+        expect(mail._private.validate_folders(legit_folder)).to.be.ok
+    })
+
+    it('should return false with a too short folder structure', () => {
+        expect(mail._private.validate_folders(not_legit_folder1)).to.be.false
+    })
+
+    it('should return false without a thread/messages field', () => {
+        expect(mail._private.validate_folders(not_legit_folder1)).to.be.false
+    })
+})
+
+
+
+
+                
