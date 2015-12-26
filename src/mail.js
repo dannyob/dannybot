6ff9332fd791558824f1baf214fa8e7c9ebe3862
Danny O'Brien <Danny O'Brien danny@spesh.com>
2015-12-26 00:48:24 -0800

    Change name of tidying input function from validate_folder to cleanup

--- src/mail.js
+++ src/mail.js
@@ -15,8 +15,8 @@ function get_mail_config(config_file, cb) {
             });
 }
 
-// enforce some structure on folders
-function validate_folders(mf) {
+// enforce some structure and object structure on folders
+function _cleanup(mf) {
     var new_mf = _.mapObject(mf, function (v) {
         if (v.length !== 3 ) { throw new Error('Mail folder definition should be three fields long.' + v); }
         if ((v[0] !== 'threads') && (v[0] !== 'messages')) { throw new Error('Mail folder should specify threads or messages.'); }
@@ -30,6 +30,8 @@ module.exports.folders = folders;
 
 if (process.env.NODE_ENV === 'test') {
     module.exports._private = {};
-    module.exports._private.validate_folders = validate_folders;
-    module.exports._private.get_mail_config = get_mail_config;
+    module.exports._private.cleanup  = _cleanup;
+    module.exports._private.get_mail_config = _get_mail_config;
+} else {
+    _get_mail_config('').then(function(d) { console.log(_cleanup(d)); } );
 }
--- test/mail.js
+++ test/mail.js
@@ -12,16 +12,16 @@ const legit_folder = { 'inbox' : ['threads', 'tag:foo', 'inbox'] };
 const not_legit_folder1 = { 'messbox' : ['threads', 'wrongsize'] };
 const not_legit_folder2 = { 'messbox' : ['scorcher', 'otherwise', 'okay'] };
 
-describe('mail._private.validate_folders', () => {
+describe('mail._private.cleanup', () => {
     it('should return value on a legit folder structure', () => {
-        expect(mail._private.validate_folders(legit_folder)).to.deep.equal(legit_folder);
+        expect(mail._private.cleanup(legit_folder)).has.property('inbox');
     });
 
     it('should throw error with a too short folder structure', () => {
-        expect(function () { mail._private.validate_folders(not_legit_folder1); }).to.throw(/three/);
+        expect(function () { mail._private.cleanup(not_legit_folder1); }).to.throw(/three/);
     });
 
     it('should throw error without a thread/messages field', () => {
-        expect(function () { mail._private.validate_folders(not_legit_folder2); }).to.throw(/thread/);
+        expect(function () { mail._private.cleanup(not_legit_folder2); }).to.throw(/thread/);
     });
 });
