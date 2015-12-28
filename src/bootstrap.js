b5a1e4ff80465d01b587d25296209620552df2e0
Danny O'Brien <Danny O'Brien danny@spesh.com>
2015-12-27 22:42:15 -0800

    Spot errors thrown in async code without await
    (Also removes code that came from belief that 'window' global variable
    wasn't exposed in require modules)

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,9 @@
 /*eslint-env browser */
 require('babel/register');
-var s = require('./statbot.js');
-window.onload = s.ready;
+
+// spot errors thrown in async code without await
+// see http://stackoverflow.com/questions/30649994/can-i-catch-an-error-from-async-without-using-await
+// http://web.archive.org/web/20151228062951/http://stackoverflow.com/questions/30649994/can-i-catch-an-error-from-async-without-using-await
+process.on('unhandledRejection', err => { throw err; });
+
+require('./statbot.js');
