0ba3574b5277d84def5e5b9a0472d0fd49ac65ff
Danny O'Brien <Danny O'Brien danny@spesh.com>
2016-03-13 00:02:58 -0800

    Rename statbot to dannybot

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,4 +6,4 @@ require('babel/register');
 // http://web.archive.org/web/20151228062951/http://stackoverflow.com/questions/30649994/can-i-catch-an-error-from-async-without-using-await
 process.on('unhandledRejection', err => { throw err; });
 
-require('./statbot.js');
+require('./dannybot.js');
